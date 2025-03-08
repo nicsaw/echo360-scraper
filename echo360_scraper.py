f7bf0849c8be486cdeabe4c22b92a0170b4741c3
SAW <99191294+nicsaw@users.noreply.github.com>
2025-03-08 19:05:15 +1100

    Download course videos

--- echo360_scraper.py
+++ echo360_scraper.py
@@ -8,6 +8,8 @@ import dotenv
 import os
 import re
 import json
+from typing import Any
+from datetime import datetime
 
 BASE_URL = "https://echo360.net.au"
 MAIN_LOGIN_URL = "https://login.echo360.net.au/login"
@@ -46,7 +48,7 @@ def expand_course_codes(course_codes_str: str) -> list[str]:
 
     return list(course_codes)
 
-def get_courses(driver: webdriver.Chrome, courses_url=COURSES_URL):
+def get_courses(driver: webdriver.Chrome, courses_url=COURSES_URL) -> list[dict[str, Any]]:
     driver.get(courses_url)
     course_cells = WebDriverWait(driver, 10).until(
         EC.presence_of_all_elements_located(
@@ -85,14 +87,76 @@ def get_courses(driver: webdriver.Chrome, courses_url=COURSES_URL):
             "link": link,
         })
 
-    for course in courses:
-        print(json.dumps(course, indent=2))
+    return courses
+
+def scrape_course(course_url: str, driver: webdriver.Chrome):
+    driver.get(course_url)
+    sessions = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "class-row"))
+    )
+
+    course_data = []
+    for session in sessions:
+        title = session.find_element(By.CSS_SELECTOR, 'div[role="title"].title').text.strip()
+        date_str = session.find_element(By.CLASS_NAME, "date").text.strip()
+        time_str = session.find_element(By.CLASS_NAME, "time").text.strip()
+
+        date_obj = datetime.strptime(date_str, "%B %d, %Y").date()
+        start_time_str, end_time_str = time_str.split("-")
+        start_time = datetime.strptime(start_time_str, "%I:%M%p").time()
+        end_time = datetime.strptime(end_time_str, "%I:%M%p").time()
+
+        # Open video menu
+        WebDriverWait(session, 10).until(
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, 'div.courseMediaIndicator[data-test-id="open-class-video-menu"]')
+            )
+        ).click()
+
+        # Click "Download Original"
+        WebDriverWait(session, 10).until(
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, 'a[data-test-id="download-class-media"]')
+            )
+        ).click()
+
+        # Click on Video 1 HD Download button
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, 'button[data-test-id="video1-hd-download"]')
+            )
+        ).click()
+
+        # Close "Download" dialog box
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, 'div[data-test-component="DownloadMedia"] button[aria-label="Close"]')
+            )
+        ).click()
+
+        course_data.append({
+            "title": title,
+            "date": date_obj,
+            "start_time": start_time,
+            "end_time": end_time,
+        })
+
+    return course_data
 
 def main():
+    options = webdriver.ChromeOptions()
+    options.enable_downloads = True
     driver = webdriver.Chrome()
 
     login(driver)
-    get_courses(driver)
+    courses = get_courses(driver)
+    for course in courses:
+        print(json.dumps(course, indent=2))
+
+    COMP4337_URL = courses[1].get("link")
+    COMP4337_lectures = scrape_course(COMP4337_URL, driver)
+    for lecture in COMP4337_lectures:
+        print(f"{lecture = }")
 
     driver.quit()
 
